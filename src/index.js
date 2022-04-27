71d8e60fdfb1702a159d790f68bcde2d5dc94765
ciera <cieranicole.m15@gmail.com>
2022-04-27 19:14:33 -0400

    Complete Deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleInput(e.target[0].value)
+    form.reset()
+  })
 });
+
+function handleInput(todo) {
+    let p = document.createElement('p')
+    p.textContent = `${todo} `
+    let btn = document.createElement('button')
+    btn.addEventListener('click', deleteBtn)
+    btn.textContent = 'X'
+    p.appendChild(btn)
+    document.querySelector('#list').appendChild(p)
+}
+function deleteBtn(e) {
+    e.target.parentNode.remove()
+}
